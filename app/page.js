6e9de7701a71575c511d204c10a06cf578b27ecb
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-05-18 16:56:24 +0530

    Oauth using google implemented

--- app/page.js
+++ app/page.js
@@ -1,32 +1,40 @@
 'use client';
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { auth } from "./firebase";
-import { GoogleAuthProvider, signInWithPopup, signInWithRedirect} from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "firebase/auth";
+import Dashboard from "./pages/dashboard";
+// import {useAuthState} from 'react-firebase-hooks/auth';
+// const router = useRouter();
 export default function Home() {
 
 
   const provider = new GoogleAuthProvider();
 
 
-  
-  
-
   const handleLogin = async () => {
+    const [user, setUser] = useState(auth);
+    try {
+      const result = await signInWithPopup(auth, provider)
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // console.log(user);
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    }
 
-    const result = await signInWithPopup(auth, provider)
-    // .then((result) => {
-    //   const credential = GoogleAuthProvider.credentialFromResult(result);
-    //   const token = credential.accessToken;
-
-    //   const user = result.user;
-    // }).catch((error) => {
-    //   const errorCode = error.code;
-    //   const errorMessage = error.message;
-
-    //   const email = error.customData.email;
-
-    //   const credential = GoogleAuthProvider.credentialFromError(error)
-    // });
+   
+    
   }
+
+
 
 
 
