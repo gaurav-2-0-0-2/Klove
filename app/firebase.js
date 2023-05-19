efd8480d666450bb521b4340a5f16448e0a85bb9
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-05-19 10:41:41 +0530

    removed trash code

--- app/firebase.js
+++ app/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
+
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,3 +24,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 // const analytics = getAnalytics(app);
+
+
+
+
+
+
+
+
