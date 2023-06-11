74e34100298a573c5d9988bf19f5b55ed304f55e
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-06-11 15:10:29 +0530

    added firestore

--- firebase.config.js
+++ firebase.config.js
@@ -1,7 +1,22 @@
 // Import the functions you need from the SDKs you need
-import {getApp, getApps, initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
-import {getStorage} from 'firebase/storage';
+import { getApp, getApps, initializeApp } from "firebase/app";
+// import { getFirestore } from 'firebase/firestore';
+// import { getStorage } from 'firebase/storage';
+// import { initializeApp } from "firebase/app";
+import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithPopup,
+    signOut,
+} from "firebase/auth";
+import {
+    getFirestore,
+    query,
+    getDocs,
+    collection,
+    where,
+    addDoc,
+} from "firebase/firestore";
 // import { getAuth } from 'firebase/auth';
 
 
@@ -14,23 +29,56 @@ import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain:process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId:process.env.NEXT_PUBLIC_PROJECT_ID,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    appId:process.env.NEXT_PUBLIC_APP_ID ,
-    measurementId:process.env.NEXT_PUBLIC_MEASUREMENT_ID
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 // export const auth = getAuth(app);
 // const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+// const storage = getStorage(app);
 
-const firestore = getFirestore(app);
-const storage = getStorage(app);
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async ()=>{
+    try{
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        const q = query(collection(db, "users"), where("uid", "==" ,user.uid));
+        const docs = await getDocs(q);
+        if(docs.docs.length === 0){
+            await addDoc(collection(db, "users"), {
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email,
+                image: user.photoURL,
+            });
+        }
+    }catch (err){
+        console.error(err);
+    }
+};
 
-export {app, firestore, storage};
+
+const logout = () =>{
+    signOut(auth);
+}
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logout,
+};
+
+
 
 
 
