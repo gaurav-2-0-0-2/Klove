68b35afaf094f47e480956c4cb91d0e73ac60a72
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-06-12 16:12:21 +0530

    fetching user from firestore

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,29 +1,51 @@
-
-import {useAuthState} from "react-firebase-hooks/auth";
+"use client";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db, logout } from "../../firebase.config";
 import { query, collection, getDocs, where } from "firebase/firestore";
+import { useEffect } from "react";
 
 export default function Dashboard() {
 
     // const [userData, setUserData] = useState('');
 
-    // const fetchUserData = async ()=>{
-    //         try {
-            
-    //             let q = query(collection(db, 'users'), where("uid", "==", user?.uid));
-    //             const doc = await getDocs(q);
-    //             const data = doc.docs.data();
-    //             console.log(data);
 
-    //         } catch (err) {
-    //             console.error(err);                
-    //         }
-    // }
+    const fetchUsers = async () => {
+
+        try {
+            const q = query(collection(db, "users"));
+
+            const querySanpshot = await getDocs(q);
+
+            const data =  querySanpshot.data();
+
+            console.log(data);
+
+            // querySanpshot.forEach((doc) => {
+            //     console.log(doc.id, "=>", doc.data());
+            // })
+
+        } catch (err){
+            console.error(err)
+        }
+
+
+
+
+    }
+
+
+
+
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
+
 
     return (
         <div>
             <h1>This is user's Dashboard</h1>
-
+            {/* <button onClick={}>Get Doc</button> */}
         </div>
     )
 } 
