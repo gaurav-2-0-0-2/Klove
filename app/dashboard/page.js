b80bad6bf93bdc6c7b7296bfb0feb32421a2d65f
Gaurav Tak <gauravtak996@gmail.com>
2023-06-20 15:20:02 +0530

    added textarea using (?:)

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,12 @@
 "use client";
-import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db, logout } from "../../firebase.config";
 import { query, collection, getDocs, where } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import { FaUser, FaHome, FaSearch, FaEdit } from "react-icons/fa"
+import { Input } from "antd";
+const { TextArea } = Input;
 
 
 // Made this a prrotected route 
@@ -13,6 +14,8 @@ import { FaUser, FaHome, FaSearch, FaEdit } from "react-icons/fa"
 export default function Dashboard() {
 
     const router = useRouter();
+
+    const [textarea, setTextarea] = useState(false);
 
     const { currentUser, signout } = useAuth();
 
@@ -30,8 +33,13 @@ export default function Dashboard() {
         router.push("/");
     }
 
+
+    const handleClick = () => {
+        setTextarea(true);
+    }
+
     return (
-        <div>
+        <div className="relative">
 
             <div className="flex flex-col text-center h-screen max-w-[10rem] bg-maroon-light px-6">
                 <nav className="flex-1 mt-6">
@@ -39,10 +47,6 @@ export default function Dashboard() {
                         {/* <FaUser size={30} className="inline-block text-16" /> */}
                         {/* <a href="#" className="block text-gray-900">User</a> */}
                         <img src={currentUser.photoURL} className="inline-block rounded-full w-[50px]" alt="profile-pic" />
-                    </div>
-                    <div className="mt-12 cursor-pointer">
-                        <FaHome size={25} className="inline-block text-16" />
-                        {/* <a href="#" className="block text-gray-900">Home</a> */}
                     </div>
                     <div className="mt-12 cursor-pointer">
                         <FaSearch size={25} className="inline-block text-16" />
@@ -59,10 +63,16 @@ export default function Dashboard() {
             </div>
 
 
-            <div className="fixed top-[40%] left-[50%] text-center">
-                <h1 className="text-light-grey text-3xl font-thin mb-10">No Activity Yet</h1>
-                <button className="bg-maroon-dark px-10 py-2 font-bold text-2xl text-white hover:bg-maroon-light drop-shadow-2xl">WRITE</button>
-            </div>
+            {textarea ? (<div>
+                <TextArea rows={25} className="w-[60%] absolute top-[10%] left-[20%]" />
+                <button className="absolute left-[70%] bottom-[5%] bg-maroon-dark px-10 py-2 font-bold text-2xl text-white hover:bg-maroon-light drop-shadow-2xl">POST</button>
+            </div>) : (
+                <div className="fixed top-[40%] left-[50%] text-center">
+                    <h1 className="text-light-grey text-3xl font-thin mb-10">No Activity Yet</h1>
+                    <button onClick={handleClick} className="bg-maroon-dark px-10 py-2 font-bold text-2xl text-white hover:bg-maroon-light drop-shadow-2xl">WRITE</button>
+                </div>
+            )}
+
 
             {/* <button onClick={}>Get Doc</button> */}
 
