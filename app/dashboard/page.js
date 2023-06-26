f6571a979a1ead15455efe56bc9b03b2875ce501
Gaurav Tak <gauravtak996@gmail.com>
2023-06-26 14:47:09 +0530

    add post succesfully on firestore and UI

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,10 +1,10 @@
 "use client";
 import { auth, db, logout, postRecipe } from "../../firebase.config";
-import { query, collection, getDocs, where, addDoc } from "firebase/firestore";
+import { query, collection, getDocs, where, addDoc, onSnapshot } from "firebase/firestore";
 import { useEffect, useState, useRef } from "react";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { FaUser, FaHome, FaSearch, FaEdit } from "react-icons/fa"
+import { FaUser, FaHome, FaSearch, FaEdit, FaHeart, FaTrash } from "react-icons/fa"
 import { Input } from "antd";
 const { TextArea } = Input;
 
@@ -19,16 +19,12 @@ export default function Dashboard() {
     const formRef = useRef();
 
     const [title, setTitle] = useState('');
-    const [recipe, setRecipe] = useState('');
-
+    const [posts, setPosts] = useState(null);
     const [textarea, setTextarea] = useState(false);
 
 
 
     const { currentUser, signout } = useAuth();
-
-
-    const posts = []; // to store users posts for frontend 
 
     useEffect(() => {
         if (currentUser) {
@@ -36,8 +32,19 @@ export default function Dashboard() {
         } else if (currentUser == null) {
             router.push("/login");
         }
-    })
-    console.log(currentUser);
+
+        // fetchPosts();
+
+
+        
+
+    }, [])
+    // console.log(currentUser);
+
+    // const fetchPosts = async () => {
+    //     const response = collection(db, 'posts');
+    //     console.log(response);
+    // }
 
     const handleLogout = () => {
         const logoutUser = signout();
@@ -63,15 +70,37 @@ export default function Dashboard() {
         }
 
         try {
-            // const querySnapshot = await getDocs(query(dbRef));
-            // if (querySnapshot.size === 0) {
+            // Adding document in posts collection on firestore
             await addDoc(dbRef, newRecipe);
-            // console.log(newRecipe);
-            posts.push(newRecipe);
-            console.log(posts);
-            // } else {
-            //     console.log('Document already exists.');
-            // }
+
+            // querying the posts collection
+        const q = query(collection(db, "posts"));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+
+            let updatePosts = [];
+            snapshot.docChanges().forEach((change) => {
+
+                if (change.type === "added") {
+                    console.log("New post: ", change.doc.data());
+                    updatePosts.push(change.doc.data());
+                    // setPosts((prevPosts) => [...prevPosts, change.doc.data()]);
+
+                }
+                // if (change.type === "modified") {
+                //     console.log("Modified post: ", change.doc.data());
+                // }
+                // if (change.type === "removed") {
+                //     console.log("Removed post: ", change.doc.data());
+                // }
+            });
+            setPosts(updatePosts);
+        });
+
+        console.log(posts);
+
+        return () => unsubscribe();
+            // posts.push(newRecipe);
+            // console.log(posts);
         } catch (err) {
             console.error(err);
         }
@@ -80,20 +109,18 @@ export default function Dashboard() {
     return (
         <div className="flex flex-row gap-10">
 
+            {/* Vertical Navbar on far left */}
+
             <div className="flex flex-col text-center h-screen max-w-[10rem] bg-maroon-light px-6">
                 <nav className="flex-1 mt-6">
                     <div className="mt-12 cursor-pointer">
-                        {/* <FaUser size={30} className="inline-block text-16" /> */}
-                        {/* <a href="#" className="block text-gray-900">User</a> */}
                         <img src={currentUser.photoURL} className="inline-block rounded-full w-[50px]" alt="profile-pic" />
                     </div>
                     <div className="mt-12 cursor-pointer">
                         <FaSearch size={25} className="inline-block text-16" />
-                        {/* <a href="#" className="block text-gray-900">Explore</a> */}
                     </div>
                     <div className="mt-12 cursor-pointer">
                         <FaEdit size={25} className="inline-block text-16" />
-                        {/* <a href="#" className="block text-gray-900">Edit</a> */}
                     </div>
 
                 </nav>
@@ -102,31 +129,51 @@ export default function Dashboard() {
             </div>
 
 
-            {posts.length === 0 ? (
-                <div className="basis-1/2 mt-4">{textarea ? (<div>
-                    <form onSubmit={postRecipe} ref={formRef}>
-                        <input type="text" placeholder="Title" />
-                        <TextArea className="text-md mt-2" rows={25} placeholder="Write your Recipe..." />
-                        <button type="submit" className="mt-2 bg-maroon-dark px-10 py-2 font-bold text-2xl text-white hover:bg-maroon-light drop-shadow-2xl">POST</button>
-                    </form>
-                </div>) : (
-                    <div className="fixed top-[40%] left-[50%] text-center">
-                        <h1 className="text-light-grey text-3xl font-thin mb-10">No Activity Yet</h1>
-                        <button onClick={handleClick} className="bg-maroon-dark px-10 py-2 font-bold text-2xl text-white hover:bg-maroon-light drop-shadow-2xl">WRITE</button>
-                    </div>
-                )}</div>
-            ) : (
-                <div>
-                    {posts.map((post) => {
+            {/* body of dashboard */}
+
+            {posts && posts.length > 0 ? (
+                <div className="grid grid-cols-3 gap-6">
+                    {posts.map((post, index) => {
                         return (
-                            <div key={post.uid}>
-                                <h1>{post.title}</h1>
-                                <p>{post.content}</p>
+                            <div key={index} className="bg-white px-4 py-4">
+                                <h1 className="bg-peach-lighter">{post.title}</h1>
+                                <p className="bg-maroon-light">{post.content}</p>
+                                <div className="flex flex-row">
+                                    <FaHeart/>
+                                    <FaTrash/>
+                                </div>
                             </div>
                         )
                     })}
                 </div>
+            ) : (
+                
+                    <div className="basis-1/2 mt-4">{textarea ? (<div>
+                        <form onSubmit={postRecipe} ref={formRef}>
+                            <input type="text" placeholder="Title" />
+                            <TextArea className="text-md mt-2" rows={25} placeholder="Write your Recipe..." />
+                            <button type="submit" className="mt-2 bg-maroon-dark px-10 py-2 font-bold text-2xl text-white hover:bg-maroon-light drop-shadow-2xl">POST</button>
+                        </form>
+                    </div>) : (
+                        <div className="fixed top-[40%] left-[50%] text-center">
+                            <h1 className="text-light-grey text-3xl font-thin mb-10">No Activity Yet</h1>
+                            <button onClick={handleClick} className="bg-maroon-dark px-10 py-2 font-bold text-2xl text-white hover:bg-maroon-light drop-shadow-2xl">WRITE</button>
+                        </div>
+                    )}</div>
+                
             )}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
