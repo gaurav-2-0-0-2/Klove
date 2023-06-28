a8637a3d604299e3e534a0663ec9186faccdc866
Gaurav Tak <gauravtak996@gmail.com>
2023-06-28 21:11:45 +0530

    added text below the heading

--- app/component/Body.js
+++ app/component/Body.js
@@ -11,14 +11,14 @@ export default function Body() {
                 <div>
                     <h1 className="hero-text1 text-7xl text-vibrant-orange">SHARE YOUR</h1>
                     <h1 className="hero-text3 text-7xl text-fresh-green">FOOD-GORITHMS</h1>
-                    <p className="w-[45%] pr-3 mt-6">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et dignissimos neque sequi veniam earum error laudantium odit vel totam doloremque necessitatibus saepe dolorem voluptatem cupiditate aliquid laborum optio, debitis voluptate!</p>
+                    <p className="w-[45%] pr-3 mt-6 text-dark-gray font-bold">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et dignissimos neque sequi veniam earum error laudantium odit vel totam doloremque necessitatibus saepe dolorem voluptatem cupiditate aliquid laborum optio, debitis voluptate!</p>
                     <div className="flex gap-10 mt-16">
                         <button className="hover:shadow-xl hover:shadow-fresh-green hover:scale-[1.1] transition ease-in-out delay-5 text-white bg-fresh-green text-xl font-extrabold px-8 py-2 rounded-md">WRITE</button>
                         <button className="hover:shadow-xl hover:shadow-vibrant-orange-dark hover:scale-[1.1] transition ease-in-out delay-5 text-white bg-vibrant-orange text-xl font-extrabold px-8 py-2 rounded-md">SIGNUP</button>
                     </div>
                 </div>
                 <div className="relative">
-                {/* The middle image */}
+                    {/* The middle image */}
                     <img
                         className=" z-10 absolute translate-y-8 translate-x-[-10rem] w-[380px] h-[350px]"
                         src='/food1.jpg'
