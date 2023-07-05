64dfbd1bb144e967b26d58b5102ffbb8e9f59574
Gaurav Tak <gauravtak996@gmail.com>
2023-07-05 21:08:26 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
+import About from "./component/About";
 import Body from "./component/Body";
 import Navbar from "./component/Navbar";
+import Overview from "./component/Overview";
+import { Nunito } from 'next/font/google';
+
+const nunito = Nunito({ 
+    subsets: ['latin'], 
+    weight: ['400', '700'],
+    variable: '--font-nunito'
+});
 
  export default function Home(props) {
 
   return (
-    <div>
+    <div className={`${nunito.variable} font-sans2`}>
       <Navbar/>
       <Body/>
+      <Overview/>
+      <About/>
     </div>
   )
 
