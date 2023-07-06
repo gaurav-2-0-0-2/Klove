3ad4c51ddf28a3b083a233be91292b62f96188f9
Gaurav Tak <gauravtak996@gmail.com>
2023-07-06 14:29:56 +0530

    need to impl. next-auth

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,32 +1,46 @@
 'use client'
-import React, {useContext, useState, useEffect, useRef, Children} from "react";
-import {auth,db, signInWithGoogle, logout} from "../firebase.config";
-import { onAuthStateChanged } from "firebase/auth";
-import {doc, getDoc} from "firebase/firestore";
+import React, { useContext, useState, useEffect, useRef } from "react";
+import { auth } from "../firebase.config";
+import {
+    onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut
+} from "firebase/auth";
+
+// use next-auth instead of firebase auth ****
 
 const AuthContext = React.createContext();
 
-export function useAuth(){
-    return useContext(AuthContext); 
+export function useAuth() {
+    return useContext(AuthContext);
 }
 
-export function AuthProvider({children}){
+const googleProvider = new GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const userInfo = useRef();
 
-    function loginWithGoogle(){
+    function loginWithGoogle() {
         return signInWithGoogle();
     }
 
-    function signout(){
-        return logout();
+    function signout() {
+        return signOut();
     }
 
 
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth, user=>{
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
             setCurrentUser(user);
             setLoading(false);
         })
