bfaf8433fa51ffb68469bc08e1bb22edcec77b12
Gaurav Tak <gauravtak996@gmail.com>
2024-02-25 13:06:52 +0530

    changed package from @apollo/server-->grapqhql-yoga

--- server/src/resolvers/resolvers.ts
+++ server/src/resolvers/resolvers.ts
@@ -1,4 +1,3 @@
-// import { PrismaContext } from "../context";
 
 const resolvers = {
   Query: {
@@ -8,7 +7,7 @@ const resolvers = {
   },
 
   Mutation:{
-    async createUser(_:any, args: { name:string, email: string; password: string; }, context:any){
+    async signup(_:any, args: { name:string, email: string; password: string; }, context:any){
       const { name, email, password} = args;
 
       const response = await context.prisma.user.create({
