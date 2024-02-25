4c7ff71dc037a47a6101ed40c935b2746c73adf0
Gaurav Tak <gauravtak996@gmail.com>
2024-02-26 00:35:07 +0530

    changed import from ts to js

--- server/src/resolvers/resolvers.ts
+++ server/src/resolvers/resolvers.ts
@@ -1,7 +1,7 @@
-import * as Query from "./Query"
-import * as Mutation from "./Mutation";
+import * as Query from "./Query.js"
+import * as Mutation from "./Mutation.js";
 const resolvers = {
   Query,
-  Mutation
+  Mutation 
 };
 export default resolvers;
